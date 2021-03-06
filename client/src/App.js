c900ec1f1488b1bcc15488f948498282a78f49e8
Bill Goers <wjgoers@gmail.com>
2021-03-06 13:32:41 -0600

    footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react"
 import './App.css'
-import Header from "./components/Header"
+import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react"
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+      <span className="navbar-brand mb-1 h1">Wendi Freeman</span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+       aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <nav className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="nav navbar-nav navbar-right">
+          <li className="nav-item">
+            <a className="nav-link" href="/Contact">Placeholder</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/Portfolio">Placeholder</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/AboutMe">Placeholder</a>
+          </li>
+        </ul>
+      </nav>
+    </nav>
+    )
+}
+export default Navbar
