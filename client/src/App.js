2a171c100771635ffcde5167839f4e8ca18e9cf6
Bill Goers <wjgoers@gmail.com>
2021-03-09 20:01:17 -0600

    updated comps

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import React from "react"
 import './App.css'
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Profile from "./components/Profile"
+import BandSearch from "./components/BandSearch"
+import MusicianSearch from "./components/MusicianSearch"
+import Dashboard from "./components/Dashboard"
 
 function App() {
   return (
     <div>
       <Navbar />
       <Footer />
+      <Dashboard />
+      <Profile />
+      <BandSearch />
+      <MusicianSearch />
     </div>
   );
 }
---
+++ client/src/components/MusicianSearch.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function MusicianSearch() {
+
+}
+
+export default MusicianSearch
