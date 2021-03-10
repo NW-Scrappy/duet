9d383cc9c27af16d9309abcfa9ce24c4ce0c66ab
Bill Goers <wjgoers@gmail.com>
2021-03-10 13:45:38 -0600

    updated components

---
+++ client/src/ButtonComponents/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Button() {
+  return (
+    <button style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+        
+    </button>
+  );
+}
