ba36c07049675325ce58073b734265f0767c7267
lauren levaccare <laurenlevaccare@icloud.com>
2021-03-11 11:58:09 -0600

    reg form and search form framework

--- client/src/ButtonComponents/Button.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Button() {
-  return (
-    <button style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
-        
-    </button>
-  );
-}
