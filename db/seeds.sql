d3549ce605b865d32a2645d13c3b6dea217e325c
Wendi Freeman <wfreeman2112@gmail.com>
2021-03-11 20:09:57 -0600

    seeded data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,22 @@
 use duet;
 
 insert into Bands (band_name, instrument_seeking, band_genre, band_link,createdAt, updatedAt)
-values ("Spot", "drums", "none", "lo@yahoo.com",CURDATE(), CURDATE());
-
+values("Hit N Run", "drums", "none", "lo@yahoo.com",CURDATE(), CURDATE());
+      ("Josie and the Pussycats", "bass", "pop punk", "josie@aol.com",CURDATE(), CURDATE());
+      ("The Wonders", "drums", "oldies", "oneders@gmail.com",CURDATE(), CURDATE())
 
 insert into Musicians (musician_name, instrument_playing, musician_genre, musician_link,createdAt, updatedAt)
 
 values ("ben", "guitar","rock", "l@gmail.com",CURDATE(), CURDATE());
+        ("Flea", "bass","funk", "flea@gmail.com",CURDATE(), CURDATE());
+        ("Brendan C", "drums","punk", "BCanty@gmail.com",CURDATE(), CURDATE())
 
 
 insert into users (UserName, Email,Password,createdAt, updatedAt)
 
-values ("Wendi", "Eric", "Ericishere",CURDATE(), CURDATE());
+values ("Wendi", "Eric@gmail.com", "Ericishere",CURDATE(), CURDATE());
+        ("Jordan", "JB@aol.com", "password1",CURDATE(), CURDATE());
+        ("Lauren L", "LL@gmail.com", "password2",CURDATE(), CURDATE());
 
 
 -- insert into authentication (userid, password)
