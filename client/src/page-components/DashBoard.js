e21a50fdf771d230a86d0a228a7b44105bc6a471
Bill Goers <wjgoers@gmail.com>
2021-03-18 19:36:53 -0500

    buttons for dashboard

--- client/src/page-components/DashBoard.js
+++ client/src/page-components/DashBoard.js
@@ -1,8 +1,36 @@
-import React from "react"
+import React from "react";
+import Button from "react-bootstrap/Button";
 
 function Dashboard() {
-    return(
-        <p>Dashboard</p>
-    )
+  return (
+    <div>
+      <Button
+        href="/postband"
+        className="bandBtn"
+        variant="primary"
+        type="submit"
+      >
+        Create your Band Post! 
+      </Button>
+      <Button
+        href="/SearchPage"
+        className="searchBtn"
+        variant="primary"
+        type="submit"
+      >
+        Search for a Musician/Band!
+      </Button>
+      <Button
+        href="/postmusician"
+        className="musicianBtn"
+        variant="primary"
+        type="submit"
+      >
+        Create your Musician Post!
+      </Button>
+
+
+    </div>
+  );
 }
 export default Dashboard;
